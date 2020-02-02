a8dd50f7dc15f9e29b44e1e8284d933294c52138
fuerte-nerd <fuertenerd@gmail.com>
2020-02-02 14:54:38 +0000

    homepage template added

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,47 @@
-// const path = require(`path`)
+const path = require(`path`)
 
-// // The image path in markdown files will be 'assets/yourImage.jpg' so create this function to filter it through a regex to get the filename
-// const getFilename = url => {
-//   return url.match(/(?<=\/).*/g)[0]
-// }
+// The image path in markdown files will be 'assets/yourImage.jpg' so create this function to filter it through a regex to get the filename
+const getFilename = url => {
+  return url.match(/(?<=\/).*/g)[0]
+}
 
-// // Here is where we create pages for our blog
-// exports.createPages = async ({ actions, graphql, reporter }) => {
-//   const { createPage } = actions
+// Here is where we create pages for our blog
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
   
-//   const indexTemplate = path.resolve(`src/templates/indexTemplate.js`)
+  const indexTemplate = path.resolve(`src/templates/homepage.js`)
 
-//   const homepageQuery = await graphql(`
-//     {
-//       allFile(
-//         filter: {
-//           sourceInstanceName: { eq: "content" }
-//           name: { eq: "homepage" }
-//         }
-//       ) {
-//         edges {
-//           node {
-//             childMarkdownRemark {
-//               frontmatter {
-//                 hero_image
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-//   if (homepageQuery.errors) {
-//     reporter.panicOnBuild(`Error while running GraphQL query.`)
-//     return
-//   }
-//   createPage({
-//     path: "/",
-//     component: indexTemplate,
-//     context: {
-//       hero_image: getFilename(
-//         homepageQuery.data.allFile.edges[0].node.childMarkdownRemark.frontmatter.hero_image
-//       ),
-//     },
-//   })
-// }
+  const homepageQuery = await graphql(`
+    {
+      allFile(
+        filter: {
+          sourceInstanceName: { eq: "content" }
+          name: { eq: "homepage" }
+        }
+      ) {
+        edges {
+          node {
+            childMarkdownRemark {
+              frontmatter {
+                hero_image
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (homepageQuery.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  createPage({
+    path: "/",
+    component: indexTemplate,
+    context: {
+      hero_image: getFilename(
+        homepageQuery.data.allFile.edges[0].node.childMarkdownRemark.frontmatter.hero_image
+      ),
+    },
+  })
+}
---
+++ src/templates/homepage.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import BackgroundImage from "gatsby-background-image"
+
+import { Container, Button } from "reactstrap"
+
+import "../components/styles.scss"
+
+export default function homepage({ data }) {
+  const {
+    title,
+    subtitle,
+    button_text,
+  } = data.post.edges[0].node.childMarkdownRemark.frontmatter
+
+  return (
+    <BackgroundImage
+      tag="div"
+      fluid={data.image.childImageSharp.fluid}
+      backgroundColor="#333333"
+      style={{
+        backgroundSize: "cover",
+        minHeight: "100vh",
+        display: 'flex',
+        alignItems: 'flex-end'
+      }}
+    >
+        <div className="animated fadeIn delay-1s" style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          background: `linear-gradient(to bottom left, rgba(255, 255, 255, .1), rgba(255, 255, 255, .8)`,
+          zIndex: -5,
+          overflow: 'hidden'
+        }}/>
+      <div className="p-5 mb-4 animated fadeInLeftBig delay-2s ">
+        <h1 className="display-4">{title}</h1>
+        <p className="lead">{subtitle}</p>
+        <Button href="#" color="primary" size="lg">{button_text}</Button>
+      </div>
+    </BackgroundImage>
+  )
+}
+
+export const query = graphql`
+  query($hero_image: String!) {
+    post: allFile(
+      filter: {
+        sourceInstanceName: { eq: "content" }
+        name: { eq: "homepage" }
+      }
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              subtitle
+              button_text
+            }
+          }
+        }
+      }
+    }
+    image: file(relativePath: { eq: $hero_image }) {
+      childImageSharp {
+        fluid(maxWidth: 2500, duotone: { highlight: "#fafafa", shadow: "#333333" }, toFormat: PNG) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
